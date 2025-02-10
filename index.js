4b6f2dd300966561410c5fa40cd3b133a28ae055
IlMi <ilija.milovanovic@stud.hslu.ch>
2025-02-10 19:18:24 +0100

    clean up

--- index.js
+++ index.js
@@ -12,47 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('xml-content', 'index.xml'));
 })
 
-app.post('/convertToPdf', async (req, res) => {
-    const response = await fetch('https://fop.xml.hslu-edu.ch/fop.php', {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        body: req.body,
-    });
-    const responseText = await (await response.blob()).arrayBuffer()
-    const buffer = Buffer.from(responseText)
-    fs.writeFileSync(path.resolve('temp.pdf'), buffer)
-    res.sendFile(path.resolve('temp.pdf'))
-})
-
-app.post('/updateData', (req, res) => {
-    const dataToUpdate = req.body
-    // read database xml
-    const databasePath = path.resolve('xml-content', 'database', 'database.xml');
-    const databaseXml = fs.readFileSync(databasePath, 'utf-8')
-    const xmlDocDatabase = libxmljs.parseXml(databaseXml)
-    // select node to update
-    const plantStatistics = xmlDocDatabase.get(`//plant[name="${dataToUpdate.plant}"]/statistics`);
-
-    // create new node with attribute etc.
-    plantStatistics.node('price', dataToUpdate.price).attr('date', dataToUpdate.date)
-
-    console.log(xmlDocDatabase.toString())
-
-    // validate new database against schema
-    const valid = validateDatabase(xmlDocDatabase)
-    if (!valid) {
-        res.status(400).send('Invalid XML')
-        return
-    }
-
-    // write new database.xml
-    fs.writeFileSync(databasePath, xmlDocDatabase.toString(), 'utf-8')
-
-    res.sendStatus(200)
-})
-
 //Done and tested with Feature 7
 app.post('/plants', (req, res) => {
     const newPlant = req.body;
